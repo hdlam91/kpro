7515d286a5bed40a9c3b6d930fa26e8eaf77bda1
hdlam <instagibno@gmail.com>
2013-10-23 10:22:27 +0200

    mastercontrollur

---
+++ Webassistenten_ads/Webassistenten_ads_api/Controllers/MasterController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Webassistenten_ads_api.Models;
+
+namespace Webassistenten_ads_api.Controllers
+{
+    public class MasterController : Controller
+    {
+
+        private BoligEntities1 db = new BoligEntities1();
+               //..... DO this for every table.
+        AnnonseHarBestilling annonseharbestilling = new AnnonseHarBestilling();
+        Modul modul = new Modul();
+        Produkt produkt = new Produkt();
+        ProduktHarAnnonse produktHarAnnonse = new ProduktHarAnnonse();
+        ProduktHarModul productHarModul = new ProduktHarModul();
+        Prospekt prospekt = new Prospekt();
+        ProspektHarBestilling prospektharbestilling = new ProspektHarBestilling();
+        //info into this model, which we can use to put into the tables later on.
+        RealEstateAdInfo model = new RealEstateAdInfo();
+
+        //
+        // GET: /Master/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public void putToDb()
+        {
+            prospekt.ProspektID = model.RealtorId;
+            //DO this for everything.
+
+            //then call for all the tables:
+            db.Prospekts.Add(prospekt);
+
+            //finally:
+            db.SaveChanges();
+            //method
+
+        }
+
+    }
+}
