d1059bd11bed4a526d8b5aae2b27fff286bcb6da
hdlam <instagibno@gmail.com>
2013-10-23 15:53:36 +0200

    master controller, testing some stuff

--- Webassistenten_ads/Webassistenten_ads_api/Controllers/MasterController.cs
+++ Webassistenten_ads/Webassistenten_ads_api/Controllers/MasterController.cs
@@ -7,6 +7,10 @@ using Webassistenten_ads_api.Models;
 using System.Data;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
 
 namespace Webassistenten_ads_api.Controllers
 {
@@ -34,10 +38,45 @@ namespace Webassistenten_ads_api.Controllers
             //return View();
         }
 
+
+        public ActionResult Test()
+        {
+            //return PutToDb();
+            string apiUri = Url.HttpRouteUrl("DefaultApi", new { controller = "test", });
+            ViewBag.ApiUrl = new Uri(Request.Url, apiUri).AbsoluteUri.ToString();
+
+            return View();
+        }
+
+        public HttpResponseMessage DeleteProspekt(long id)
+        {
+            Prospekt prospekt = db.Prospekts.Find(id);
+            
+
+            db.Prospekts.Remove(prospekt);
+
+            db.SaveChanges();
+            HttpResponseMessage hrm = new HttpResponseMessage();
+
+            return hrm;
+            
+        }
+
+
+        public HttpResponseMessage PutProspekt(long id, Prospekt prospekt)
+        {
+            prospekt = db.Prospekts.Find(id);
+            prospekt.BBOverskrift = "Modified";
+            db.Entry(prospekt).State = EntityState.Modified;
+            db.SaveChanges();
+            HttpResponseMessage hrm = new HttpResponseMessage();
+            
+            return hrm;
+        }
+
         public ActionResult PutToDb()
         {
             prospekt = new Prospekt();
-            //prospekt.ProspektID = 1101433;
             prospekt.PartnerID = 3;
             prospekt.BrukerID = 137;
             prospekt.DatoReg = new DateTime(2013, 10,23);
@@ -45,7 +84,7 @@ namespace Webassistenten_ads_api.Controllers
             prospekt.Oppdragsnr = "No entity add";
             prospekt.TEMP_ModulID = 3;
             prospekt.BBOverskrift = "HDTEST";
-            prospekt.Overskrift1 = "HDTEST";
+            prospekt.Overskrift1 = "TestModell";
             prospekt.InndelingID = 3;
             prospekt.BoligtypeID = 3;
             prospekt.Annonsetekst = "HD sin annonse";
@@ -53,7 +92,7 @@ namespace Webassistenten_ads_api.Controllers
             prospekt.Pris = 133713371337;
             prospekt.Postnr = "1337";
             prospekt.Poststed = "HD poststed";
-            prospekt.Megler = "HD";
+            prospekt.Megler = "Erlend";
             prospekt.MeglerEmail = "HD@HD.com";
             prospekt.MeglerTittel = "Master of the meggler";
             prospekt.MeglerAdresse = "Hjemme hos HD";
@@ -71,18 +110,24 @@ namespace Webassistenten_ads_api.Controllers
 
             //db.Entry(prospekt).State = EntityState.Modified;
             //DO this for everything.
-            //db.Entry(prospekt).State = EntityState.Added;
             //then call for all the tables:
-            db.Prospekts.Add(prospekt);
+            //db.Entry(prospekt).State = EntityState.Added;
+            //db.Prospekts.Add(prospekt);
+            System.Diagnostics.Debug.WriteLine(db.Entry(prospekt).Entity.Megler);
+            //var result = (db.Prospekts.Where(p => (p.Overskrift1 == "TestModell")).Select(p => p));
+            //var resultProcessed = (DbQuery<Prospekt>)result;
+            //Prospekt prospekt1 = resultProcessed.First();
+            System.Diagnostics.Debug.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!");
+            //System.Diagnostics.Debug.WriteLine(prospekt1.Megler);
             
             //finally:
-            db.SaveChanges();
+            //db.SaveChanges();
             //method
             return View();
 
         }
 
-
+        //Getters for db
         //public Prospekt GetProspekt(long id)
         //{
         //    prospekt = db.Prospekts.Find(id);
