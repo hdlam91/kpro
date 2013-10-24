9d9f31a874b0cae98d50819160452de0b6fa0554
hdlam <instagibno@gmail.com>
2013-10-24 11:09:24 +0200

    added some files i used for test

---
+++ Webassistenten_ads/Webassistenten_ads_api/Models/HD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http;
+
+namespace Webassistenten_ads_api.Models
+{
+    public class HD
+    {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Webassistenten_ads.Models.RealEstateAdInfo"/> class, with all non-nullable mumbers set to -1
+		/// to mark them as not set to a proper value.
+		/// </summary>
+	
+
+        public double number {get; set;}
+    }
+    
+}
