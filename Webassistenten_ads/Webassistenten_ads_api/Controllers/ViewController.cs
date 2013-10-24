942994e66f55104e572c6571c619e913280354e6
Audun Skjervold <audun.xetrov@gmail.com>
2013-10-24 13:29:52 +0200

    Created a sample ViewController with GET and POST methods

---
+++ Webassistenten_ads/Webassistenten_ads_api/Controllers/ViewController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Webassistenten_ads_api.Controllers
+{
+    public class ViewController : Controller
+    {
+        //
+        // GET: /View/
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
