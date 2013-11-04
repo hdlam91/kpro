18c3473b8bd619ace5d1ea5b98d866b3b6826ea7
Erlend Sigholt <erlendsi@stud.ntnu.no>
2013-11-04 14:12:22 +0100

    Added method documentation

--- Webassistenten_ads/Webassistenten_ads_api/Interfaces/Authentication.cs
+++ Webassistenten_ads/Webassistenten_ads_api/Interfaces/Authentication.cs
@@ -5,9 +5,18 @@ namespace Webassistenten_ads_api
 	public interface Authentication
 	{
 		
-
+		/// <summary>
+		/// Method for authenticating via an email adress.
+		/// </summary>
+		/// <returns>Authentication.</returns>
+		/// <param name="email">Email.</param>
 		Authentication EmailAuthenticate(string email);
 
+		/// <summary>
+		/// Method for authenticating via User ID.
+		/// </summary>
+		/// <returns>Authentication.</returns>
+		/// <param name="userId">User identifier.</param>
 		Authentication IdAuthenticate(int userId);
 
 	}
