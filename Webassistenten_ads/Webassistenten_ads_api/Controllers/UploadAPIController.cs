c16d0a94644be96ab59eb49399797687aedf3a87
Erlend Sigholt <erlendsi@stud.ntnu.no>
2013-11-16 13:56:19 +0100

    Small test

--- Webassistenten_ads/Webassistenten_ads_api/Controllers/UploadAPIController.cs
+++ Webassistenten_ads/Webassistenten_ads_api/Controllers/UploadAPIController.cs
@@ -223,4 +223,85 @@ namespace Webassistenten_ads_api.Controllers
 
         }
     }
+
+	public class UploadParameters
+	{
+		// Mandatory fields:
+		
+		public int RealtorId {get;  set; }
+		
+		public int ProductId { get;  set; }
+		
+		public String ResponsibleRealtor { get;  set; }
+		
+		public string Area { get;  set; }
+		
+		public int Type { get;  set; }
+		
+		public int Price { get;  set; }
+		
+		public string Location { get;  set; }
+		
+		public string Headline { get;  set; }
+		
+		public string Adress { get;  set; }
+		
+		public int ZipCode { get;  set; }
+		
+		public string ZipArea { get;  set; }
+		
+		public DateTime BookingDate { get;  set; }
+		
+		// Non-mandatory fields below:
+		
+		public int? FinnCode { get;  set; }
+		
+		public int? ContractNr { get;  set;}
+		
+		public float? PRom { get;  set; }	// TODO: Translate acronyms to English 
+		
+		public float? Boa { get;  set; }
+		
+		public float? Bta { get;  set; }
+		
+		public float? Bra { get;  set; }
+		
+		public int? Costs { get;  set; }
+		
+		public int? PurchaseCosts { get;  set; }
+		
+		public int? CommonCosts { get;  set; }
+		
+		public int? AmountSharedDebt { get;  set; }
+		
+		public int? CommonExpenses { get;  set; }
+		
+		public int? PropertyArea { get;  set; }
+		
+		public int? PropertyType { get;  set; }
+		
+		public DateTime? ConstructionYear { get;  set; }
+		
+		public int? Floor { get;  set; }
+		
+		public int? Bedrooms { get;  set; }
+		
+		public int? Rooms { get;  set; }
+		
+		public DateTime? OpenHouseDate { get;  set; }	// Visningdato
+		
+		public String OpenHouseText { get;  set; }	// TODO: Consider better representation than String
+		
+		public String RealEstAgentName { get;  set; }
+		
+		public String RealEstAgentTitle { get;  set; }
+		
+		public int? RealEstAgentMobile { get;  set; }
+		
+		public int? RealEstAgentPhone { get;  set; }
+		
+		public String RealEstAgentEmail { get;  set; }
+		
+		public String AdText { get;  set; }	// Is this the best representation for the text?
+	}
 }
